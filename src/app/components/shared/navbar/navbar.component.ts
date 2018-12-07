f009428b31712d4e41b2552931c2bbce61f2e7bf
sinsit1 <alexandra8989@gmail.com>
2018-12-07 18:52:58 +0100

    first commit

---
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html'
+})
+export class NavbarComponent implements OnInit {
+
+  constructor( private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  buscarheroe(termino: string) {
+  this.router.navigate
+  (['/busqueda', termino])
+  }
+
+}
